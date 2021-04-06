c7efe5c15523d27bfaae7ea83d4a97115ea3855e
penglingss <manypeopl_e@naver.com>
2021-04-07 08:59:41 +0900

    remove intellij ignored file yesyes

--- src/main/java/com/mySpring/springEx/member/service/MemberService.java
+++ src/main/java/com/mySpring/springEx/member/service/MemberService.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-package com.mySpring.springEx.member.service;
-
-import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.dao.DataAccessException;
-
-import com.mySpring.springEx.member.vo.MemberVO;
-
-public interface MemberService {
-	public List listMembers() throws DataAccessException;
-
-	/* public int addMember(MemberVO memberVO) throws DataAccessException; */
-	public int removeMember(String id) throws DataAccessException;
-
-	public MemberVO login(MemberVO memberVO) throws Exception;
-
-	
-	//»õ·Î Ãß°¡µÈ ³ðµé
-	public void check_id(String id, HttpServletResponse response) throws Exception;
-
-	public void check_email(String email, HttpServletResponse response) throws Exception;
-
-	public int join_member(MemberVO member, HttpServletResponse response) throws Exception;
-
-	public void approval_member(MemberVO member, HttpServletResponse response) throws Exception;
-
-	public String create_key() throws Exception;
-
-	public void send_mail(MemberVO member) throws Exception;
-}
-=======
 package com.mySpring.springEx.member.service;
 
 import java.util.List;
@@ -50,7 +16,7 @@ public interface MemberService {
 	public MemberVO login(MemberVO memberVO) throws Exception;
 
 	
-	//»õ·Î Ãß°¡µÈ ³ðµé
+	//ï¿½ï¿½ï¿½ï¿½ ï¿½ß°ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½
 	public void check_id(String id, HttpServletResponse response) throws Exception;
 
 	public void check_email(String email, HttpServletResponse response) throws Exception;
@@ -64,4 +30,3 @@ public interface MemberService {
 	public void send_mail(MemberVO member) throws Exception;
 
 }
->>>>>>> c6667ce518f87af0b342a24ca89d7cc9838a9df4
