f8aac4c159ec99f7a57e53cb3ca5b0a6ecc6cd81
suchan <lovetmfqud@naver.com>
2021-04-14 10:34:15 +0900

    notice added

--- src/main/java/com/mySpring/springEx/common/pagination/mapper/PageMapper.java
+++ src/main/java/com/mySpring/springEx/common/pagination/mapper/PageMapper.java
@@ -8,24 +8,21 @@ import com.mySpring.springEx.common.pagination.Pagination;
 
 public interface PageMapper {
 
-	//������
+	//생성자
 	public List<Map<String, Object>> SelectAllList() throws Exception;
 
-	// Paging ��ü �����Ϳ� ���� ����¡ ��ü�� �־� ����¡ ó���� �� �� Map�������� �޾Ƽ� List�� ����
-	// ��üȭ�� ���ѵ����� ����, ������ ����¡ ó���� ���õ� �κ��� ��ü ���Ը����� ó���� �����ϵ��� ����
+	// Paging 전체 데이터에 대한 페이징 객체를 넣어 페이징 처리를 한 후 Map형식으로 받아서 List에 저장
+		// 객체화를 시켜둠으로 인해, 앞으로 페이징 처리에 관련된 부분을 객체 삽입만으로 처리가 가능하도록 구성
 	public List<Map<String, Object>> SelectAllList(Pagination pagination) throws Exception;
 	
-	//������û����
+	//수강신청내역
 	public List<Map<String, Object>> SelectAllCourseApplyList() throws Exception;
 	public List<Map<String, Object>> selectAllCourseApplyList(Pagination pagination) throws Exception;
 	public int testTableCountCT() throws Exception;
 	
-	// count ��ü ������ ������ Ȯ���ϱ� ���� �κ�����, ������ Ȯ���ϱ� ������ int�� ����
+	// count 전체 데이터 갯수를 확인하기 위한 부분으로, 갯수를 확인하기 때문에 int로 받음
 	public int testTableCount() throws Exception;
 	
-	/*----------------------------------------------------------*/
-	/*-------------------��Ʈ��-----------------------------------*/
-	/*----------------------------------------------------------*/
 	
 	//공지사항 페이징
 		public List<Map<String, Object>> NoticeAllList() throws Exception;
